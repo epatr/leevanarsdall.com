e736979505113f7737101dcb6fe24bf8e95f5730
Eric Patrick <erstpa@gmail.com>
2015-04-17 13:20:52 -0400

    Removed first three photos, re-arranged columns

--- public/pages/gallery-street.php
+++ public/pages/gallery-street.php
@@ -1,21 +1,18 @@
 	<div class="gallery">
 		<div class="row gallery-grid">
-			<div class="three columns"><a href="/gallery/street/1"><img src="/images/galleries/street/1.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/2"><img src="/images/galleries/street/2.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/3"><img src="/images/galleries/street/3.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/4"><img src="/images/galleries/street/4.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/4"><img src="/images/galleries/street/4.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/5"><img src="/images/galleries/street/5.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/6"><img src="/images/galleries/street/6.jpg" alt="" class="u-max-full-width"></a></div>
 		</div>
 		<div class="row gallery-grid">
-			<div class="three columns"><a href="/gallery/street/5"><img src="/images/galleries/street/5.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/6"><img src="/images/galleries/street/6.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/7"><img src="/images/galleries/street/7.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/8"><img src="/images/galleries/street/8.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/7"><img src="/images/galleries/street/7.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/8"><img src="/images/galleries/street/8.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/10"><img src="/images/galleries/street/10.jpg" alt="" class="u-max-full-width"></a></div>
 		</div>
 		<div class="row gallery-grid">
-			<div class="three columns"><a href="/gallery/street/10"><img src="/images/galleries/street/10.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/11"><img src="/images/galleries/street/11.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/12"><img src="/images/galleries/street/12.jpg" alt="" class="u-max-full-width"></a></div>
-			<div class="three columns"><a href="/gallery/street/14"><img src="/images/galleries/street/14.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/11"><img src="/images/galleries/street/11.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/12"><img src="/images/galleries/street/12.jpg" alt="" class="u-max-full-width"></a></div>
+			<div class="four columns"><a href="/gallery/street/14"><img src="/images/galleries/street/14.jpg" alt="" class="u-max-full-width"></a></div>
 		</div>
 		<div class="row gallery-grid">
 			<div class="one-half column"><a href="/gallery/street/9"><img src="/images/galleries/street/9.jpg" alt="" class="u-max-full-width"></a></div>
