904a680f1ff024ec7afada27856b9e0cba200abb
Eric Patrick <erstpa@gmail.com>
2015-04-17 14:20:02 -0400

    Added link to Google Doc resume

--- public/pages/about.php
+++ public/pages/about.php
@@ -1,19 +1,26 @@
 <div class="biography row">
-	<h3>Bio</h3>
-		<p>Lee Van Arsdall currently resides in Jacksonville, Florida where he earned a BFA with a 
-		concentration in photography from the University of North Florida. 
-		He works as a Graphic Designer in Ponte Vedra Beach, Florida. 
-		His current personal work deals with fabricating photographic images that question 
-		the static notion of time in a photograph and the insistence that photographs are 
-		transcriptions of reality. He also enjoys working with alternative cameras and printing 
-		methods to create works that push the boundaries of traditional photographic techniques.
-		</p>
+	<div class="eight columns">
+		<h3>Bio</h3>
+			<p>Lee Van Arsdall currently resides in Jacksonville, Florida where he earned a BFA with a 
+			concentration in photography from the University of North Florida. 
+			He works as a Graphic Designer in Ponte Vedra Beach, Florida. 
+			His current personal work deals with fabricating photographic images that question 
+			the static notion of time in a photograph and the insistence that photographs are 
+			transcriptions of reality. He also enjoys working with alternative cameras and printing 
+			methods to create works that push the boundaries of traditional photographic techniques.
+			</p>
 
-	<h3>Contact</h3>
-		<p>386.747.4766</p>
-		<p><a href="mailto:leevanarsdall@gmail.com">leevanarsdall@gmail.com</a></p>
+		<h3>Contact</h3>
+			<ul class="contact">
+				<li>386.747.4766</li>
+				<li><a href="mailto:leevanarsdall@gmail.com">leevanarsdall@gmail.com</a></li>
+				<li><a href="https://drive.google.com/file/d/0B7wxV_72JPWKM3Fyd1FkX0FtZTQ/view?usp=sharing" target="_blank" class="pdflink">Download Resume</a> (<a href="/resume/">view</a>)</li>
+			</ul>
 
-		<section id="resume-download">
-			<a href="https://drive.google.com/file/d/0B7wxV_72JPWKZFB4STYtNThYWWc/view" target="_blank">Click here to view/download in Google Drive</a>
-		</section>
+
+		</div>
+
+		<div class="four columns">
+			<img src="images/lee_van_arsdall.jpg" alt="Lee Van Arsdall" class="u-max-full-width">
+		</div>
 </div>
