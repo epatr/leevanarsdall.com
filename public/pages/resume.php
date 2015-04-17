21b183333e811cf7612655bf926806bb1e7cbf8e
Eric Patrick <erstpa@gmail.com>
2015-04-17 13:07:24 -0400

    Initial commit

---
+++ public/pages/resume.php
@@ -0,0 +1,94 @@
+<section id="education" class="twelve columns">
+  <h4>Education</h4>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2015</h6>
+</section>
+<section class="ten columns info">
+  <p>BFA, Photography Concentration, University of North Florida, Jacksonville.</p>
+</section>
+</div>
+<div class="row">
+<section id="employment" class="twelve columns">
+  <h4>Employment</h4>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2010-2015</h6>
+</section>
+<section class="ten columns info">
+  <p>Photographer & Graphic Artist, Tough Gear Inc., Ponte Vedra Beach, Florida.</p>
+  <p>	(Responsible for Web Design, Graphic Design, and Product Photography.)</p>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2014-2015</h6>
+</section>
+<section class="ten columns info">
+  <p>Student Assistant, Photo Lab, Art & Design, University of North Florida, Jacksonville.</p>
+  <p>	(Responsible for assisting students and faculty, maintaining digital and analog labs, photography studio, mixing chemistry.)</p>
+</section>
+</div>
+<div class="row">
+<section id="exhibitions" class="twelve columns">
+  <h4>Exhibitions</h4>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2015</h6>
+</section>
+<section class="ten columns info">
+  <p>Light Sensitive VII, University of North Florida Photography, Southlight Gallery, Jacksonville, FL.</p>
+  <p>	The Senior Annual, Gallery of Art, University of North Florida, Jacksonville. </p>
+  <p>	Dark Matters, UNF Student Photography Exhibition, Southlight Gallery, Jacksonville, FL.</p>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2014</h6>
+</section>
+<section class="ten columns info">
+  <p>Urban Spaces, UNF Student Photography Exhibition, Southlight Gallery, Jacksonville, FL.</p>
+  <p>	Best of UNF 2014, UNF Student Photography Exhibition, Monroe Gallery, Jacksonville, FL. </p>
+  <p>	Ctrl+Alt+Image, UNF Student Photography Exhibition, Southlight Gallery, Jacksonville, FL. </p>
+  <p>	Student Juried Annual Exhibition, Gallery of Art, University of North Florida, Jacksonville</p>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2009</h6>
+</section>
+<section class="ten columns info">
+  <p>Student Juried Annual Exhibition, Gallery of Art, University of North Florida, Jacksonville</p>
+</section>
+</div>
+<div class="row">
+<section id="publications" class="twelve columns">
+  <h4>Publications</h4>
+</section>
+</div>
+<div class="row">
+<section id="date" class="two columns">
+  <h6>2014</h6>
+</section>
+<section class="ten columns info">
+  <p>Sweat, Zach, A look into UNF’s Photo Department, Void Magazine, Issue 49, Jacksonville, FL.<br>
+  (reproductions, pps. 66-67) </p>
+  <p>	Sea of Blue Memorial March, Jacksonville, Fl. (Local publication of John Bracey)</p>
+</section>
+</div>
+<div class="row">
+<section id="references" class="twelve columns">
+  <h4>References</h4>
+</section>
+</div>
+<div class="row">
+<section class="twelve columns">
+  <p>Daryl Walls, Vice President Operations, Tough Gear Inc.</p>
+</section>
+</div>
