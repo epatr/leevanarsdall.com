b790f835381f444c57af138b8d8ef75438b7dbae
Eric Patrick <erstpa@gmail.com>
2015-04-17 14:19:43 -0400

    Made it have a gallery name, not strtoup

--- public/pages/view_photo.php
+++ public/pages/view_photo.php
@@ -5,5 +5,5 @@
 	<img src="/images/galleries/<?php echo $img_url; ?>" class="u-max-full-width">
 </div>
 <div class="row photo-breadcrumb">
-	<a href="/gallery/<?php echo $gallery_name; ?>">Back to <?php echo ucfirst($gallery_name); ?></a>
+	<a href="/gallery/<?php echo $gallery_name; ?>">Back to <?php echo $gallery; ?></a>
 </div>
